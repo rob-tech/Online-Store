2c0127f58bf729b09a068306b5b0994c706492dc
rob <robertahuber88@gmail.com>
2019-07-25 18:59:09 +0200

    V 2

--- Fetch.js
+++ Fetch.js
@@ -7,3 +7,12 @@ getProducts = async () => {
     return response
 }
 
+getProduct = async id => {
+    let url = ` https://cors-anywhere.herokuapp.com/https://strive-school-testing-apis.herokuapp.com/api/product/${id}`;
+    let headers = new Headers();
+    headers.set('Authorization', 'Basic ' + 'dXNlcjc6M1VVNWRZRnZlblJ1UlA3RQ==')
+    var response = await fetch(url, { method: 'PUT', headers: headers });
+    response = await response.json();
+    return response
+}
+
